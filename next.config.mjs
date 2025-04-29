71dc2e667f763ba215dcbf9507961d30c1b5908b
kens-pro <kens-pro@mx.biwa.ne.jp>
2025-04-29 18:02:26 +0900

    ビルド結果をプッシュ

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'docs', // この行を追加
   basePath: '/mercari001_project',
   assetPrefix: '/mercari001_project',
 }
